50f618ea137b90e9d6cfe1967b9b8baeb36dd6c0
Bruno Sousa <brunoh.henriqueh01@gmail.com>
2023-02-17 20:42:31 -0300

    chore: :lipstick: extend tailwind classes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,20 @@ module.exports = {
       backgroundImage: {
         banner: 'url(src/assets/img/banner-bg.png)',
       },
+      fontFamily: {
+        centraRegular: ['Centra No2 Book', 'sans-serif'],
+        centraMedium: ['Centra No2 Medium'],
+        centraBold: ['Centra No2 Bold'],
+      },
+      keyframes: {
+        updown: {
+          '0%': { transform: 'translateY(0)' },
+          '100%': { transform: 'translateY(-50px)' },
+        },
+      },
+      animation: {
+        'updown-slow': 'updown 2s linear infinite alternate',
+      },
     },
   },
   plugins: [],
