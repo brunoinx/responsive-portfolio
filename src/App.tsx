44d9a5e68fc89b0e442c3e9a3c88ca42c6c63106
Bruno Sousa <brunoh.henriqueh01@gmail.com>
2023-02-20 10:52:16 -0300

    fix: :bug: change structure html for banner height fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ import { Skills } from './components/organisms/Skills';
 
 function App() {
   return (
-    <div className="w-full h-screen bg-banner bg-cover">
-      <div className="max-w-[1320px] mx-auto py-4 flex flex-col justify-between">
-        <Navbar />
-        <Banner />
+    <>
+      <div className="w-full h-screen bg-banner bg-cover">
+        <div className="max-w-[1320px] mx-auto py-4 flex flex-col justify-between">
+          <Navbar />
+          <Banner />
+        </div>
       </div>
 
       <Skills />
-    </div>
+    </>
   );
 }
 
