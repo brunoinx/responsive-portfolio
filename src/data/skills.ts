b8eb1351cf05660d6cd156619048e9ddfc854c93
Bruno Sousa <brunoh.henriqueh01@gmail.com>
2023-02-26 22:02:18 -0300

    feat: :sparkles: skills data

---
+++ src/data/skills.ts
@@ -0,0 +1,27 @@
+export const skills = [
+  {
+    id: 0,
+    name: 'React JS',
+    percentage: 85,
+  },
+  {
+    id: 1,
+    name: 'React Native',
+    percentage: 85,
+  },
+  {
+    id: 2,
+    name: 'Node JS',
+    percentage: 45,
+  },
+  {
+    id: 3,
+    name: 'Typescript',
+    percentage: 50,
+  },
+  {
+    id: 4,
+    name: 'Git',
+    percentage: 92,
+  },
+];
