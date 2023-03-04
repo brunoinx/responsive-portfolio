d246b4a0bec376af928fa4d539086767ee9d1281
Bruno Sousa <brunoh.henriqueh01@gmail.com>
2023-03-04 10:20:03 -0300

    chore: :wrench: add keyframe fade animation in tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -28,9 +28,21 @@ module.exports = {
           '0%': { transform: 'translateY(0)' },
           '100%': { transform: 'translateY(-50px)' },
         },
+        fadeInTop: {
+          '0%': {
+            transform: 'translateY(-50px)',
+            opacity: 0,
+          },
+          '100%': {
+            transform: 'translateY(0)',
+            opacity: 1,
+          },
+        },
       },
       animation: {
         'updown-slow': 'updown 2s linear infinite alternate',
+        'fade-in-top':
+          'fadeInTop 0.4s cubic-bezier(0.390, 0.575, 0.565, 1.000) both',
       },
     },
   },
